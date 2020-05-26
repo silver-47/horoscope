4097a575995e996c4f329e9bbeca2009ead197b0
silver-47 <arnab.ganguly0047@gmail.com>
2020-05-26 14:50:40 +0530

    adding form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Form from './Form'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="Header">Horoscope</header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="Header">Horoscope</header>
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default App;
